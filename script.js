219b02e38b5805ab4bca4a45126f2e5c40f3b4ff
Petros Tsavdaridis <peteropon@gmail.com>
2019-04-28 13:43:53 +0200

    changed sames.value to sames.count, added bonus field which shows if 63 sum has been reached

--- script.js
+++ script.js
@@ -23,38 +23,36 @@ const store = new Vuex.Store({
                 locked: false
             }],
         dievalue: 0,
-        //ones: 0,
         sames: [
             {
                 id: 1,
-                value: 0
+                count: 0
             },
             {
                 id: 2,
-                value: 0
+                count: 0
             },
             {
                 id: 3,
-                value: 0
+                count: 0
             },
             {
                 id: 4,
-                value: 0
+                count: 0
             },
             {
                 id: 5,
-                value: 0
+                count: 0
             },
             {
                 id: 6,
-                value: 0
+                count: 0
             }
         ],
         samesTotal: 0
     },
     mutations: {
         rollDice(state) {
-            //state.sames = 0
             for (i = 0; i < state.dice.length; i++) {
                 state.dievalue = Math.floor(Math.random() * 6) + 1;
                 if (state.dice[i].locked) continue
@@ -64,19 +62,21 @@ const store = new Vuex.Store({
         countNumbers(state, n) {
             for (i = 0; i < state.dice.length; i++) {
                 if (state.dice[i].locked && state.dice[i].value === n) {
-                    state.sames[n - 1].value++
+                    state.sames[n - 1].count++
                 }
                 else continue
             }
-            state.samesTotal += (state.sames[n - 1].value * n)
+            state.samesTotal += (state.sames[n - 1].count * n)
         },
         calculateSum(state, getters) {
             for (i = 0; i < getters.getSames.length; i++) {
-                state.totalsum += getters.getSames[i].value
+                state.totalsum += getters.getSames[i].count
             }
             return state.totalsum
+        },
+        checkPairs(state) {
+
         }
-        //filter(die => die.value == n)
     },
     actions: {
         countNumbers({ commit }) {
@@ -114,9 +114,10 @@ Vue.component('dice', {
 })
 
 Vue.component('firstsum', {
-    template: `
-    <div v-show="getSum > 63"> <label> Sum </label>  {{ getSum }} </div>
-    `,
+    template: `<section>
+    <div v-show="getSum > 63"> <label> Sum </label>  {{ getSum }} </div> <br>
+    <div> <label>Bonus</label> <span v-if="getSum > 63">50 </span> <span v-else>0</span> </div>
+    </section>`,
     computed: {
         getSum() {
             return this.$store.getters.getSamesTotal
@@ -128,7 +129,7 @@ Vue.component('ettor', {
     template: `<div class="child"><button @click="countNumbers">Ones</button> <span v-show="getOnes > 0"> {{ getOnes }} </span> </div>`,
     computed: {
         getOnes() {
-            return this.$store.getters.getSames[0].value
+            return this.$store.getters.getSames[0].count
         }
     },
     methods: {
@@ -142,7 +143,7 @@ Vue.component('tvor', {
     template: `<div class="child"><button @click="countNumbers">Twos</button> <span v-show="getTwos > 0"> {{ getTwos }} </span></div>`,
     computed: {
         getTwos() {
-            return this.$store.state.sames[1].value*2
+            return this.$store.state.sames[1].count * 2
         }
     },
     methods: {
@@ -156,7 +157,7 @@ Vue.component('treor', {
     template: `<div class="child"><button @click="countNumbers">Threes</button> <span v-show="getThrees > 0"> {{ getThrees }} </span></div>`,
     computed: {
         getThrees() {
-            return this.$store.state.sames[2].value*3
+            return this.$store.state.sames[2].count * 3
         }
     },
     methods: {
@@ -170,7 +171,7 @@ Vue.component('fyror', {
     template: `<div class="child"><button @click="countNumbers">Fours</button> <span v-show="getFours > 0"> {{ getFours }} </span></div>`,
     computed: {
         getFours() {
-            return this.$store.state.sames[3].value*4
+            return this.$store.state.sames[3].count * 4
         }
     },
     methods: {
@@ -184,7 +185,7 @@ Vue.component('femor', {
     template: `<div class="child"><button @click="countNumbers">Fives</button> <span v-show="getFives > 0"> {{ getFives }} </span></div>`,
     computed: {
         getFives() {
-            return this.$store.state.sames[4].value*5
+            return this.$store.state.sames[4].count * 5
         }
     },
     methods: {
@@ -198,12 +199,22 @@ Vue.component('sexor', {
     template: `<div class="child"><button @click="countNumbers">Sixes</button> <span v-show="getSixes > 0"> {{ getSixes }} </span></div>`,
     computed: {
         getSixes() {
-            return this.$store.state.sames[5].value*6
+            return this.$store.state.sames[5].count * 6
         }
     },
     methods: {
         countNumbers() {
             store.commit('countNumbers', 6)
+        }
+    }
+})
+
+Vue.component('onepair', {
+    template: `<div class="child"><button @click="checkPair">1 pair</button> <span v-show="getSixes > 0"> {{  }} </span></div>`,
+    computed: {},
+    methods: {
+        checkPair() {
+
         }
     }
 })
